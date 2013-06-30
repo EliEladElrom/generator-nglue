b0bbab9069f0efee3d19304db32b0fd12ab0b1f0
Elad Elrom <elad.ny@gmail.com>
2013-06-29 23:25:24 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Generator-nglue
+[![Build Status](https://secure.travis-ci.org/EladElrom/generator-nglue.png?branch=master)](https://travis-ci.org/EladElrom/generator-nglue)
+
+Although it is a bit opinionated, it is not a *boilerplate* code.  This is `gruntjs` tasks to help build and manage large `Angularjs` projects that includes many re-usable components and apps that glow these re-usable components together.  The problem this project is solving is to be able to create stand alone re-usable components, but also glow them together in an app.
+An app can be anything; a page on a site, a mobile app, component that include few sub-component.  This project holds the basics and meant to be simplify so you can suite the project to your exact needs. It helps create structure to your project so member of the team can dive in and start working and deploy quickly.
+
+To get started, run grunt and bower commands:
+
+## Getting started
+- Make sure you have [yo](https://github.com/yeoman/yo) installed:
+    `npm install -g yo`
+- Install the generator: `npm install -g generator-nglue`
+- Run: `yo nglue`
+
+## License
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,64 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var NglueGenerator = module.exports = function NglueGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(NglueGenerator, yeoman.generators.Base);
+
+NglueGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'moduleName',
+    name: 'projectName',
+    message: 'What\'s your project\'s name?',
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.moduleName = props.moduleName;
+    this.projectName = props.projectName;
+
+    cb();
+  }.bind(this));
+};
+
+NglueGenerator.prototype.app = function app() {
+  this.mkdir('code_base');
+  this.mkdir('code_base/apps');
+  this.mkdir('code_base/apps/mobile');
+  this.mkdir('code_base/apps/web');
+  this.mkdir('code_base/assets');
+  this.mkdir('code_base/assets/bower_components');
+  this.mkdir('code_base/assets/components');
+  this.mkdir('code_base/assets/css');
+  this.mkdir('code_base/assets/fonts');
+  this.mkdir('code_base/assets/images');
+  this.mkdir('code_base/modules');
+  this.mkdir('compile');
+  this.mkdir('code_base/dist');
+  this.mkdir('dist');
+
+  this.copy('_.bowerrc', '.bowerrc');
+
+  this.template('_package.json', 'package.json');
+  this.template('_bower.json', 'bower.json');
+};
+
+NglueGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
