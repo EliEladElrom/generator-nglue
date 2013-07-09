1149b87a5a384c767f0aa278103cce3f1776870e
Matthew Kleiman <bigmickey@gmail.com>
2013-07-09 14:54:46 -0400

    intuitive comments

--- module/templates/_ModuleInterface.js
+++ module/templates/_ModuleInterface.js
@@ -3,12 +3,16 @@
 angular.module('<%= moduleName %>')
   .factory('<%= moduleName %>Interface', ['$rootScope', function($rootScope) {
 
-    var serviceAPI = {
-    };
+    // public API for this service, implement below
+    var serviceAPI = {};
 
+    // Private API
+    // Add any run-once code and any vars or functions private to this service
+
+
+    // expose any functions and vars publicly by adding them to serviceAPI
     serviceAPI = angular.extend(serviceAPI, {
 
     });
-
-    return serviceAPI;
+    return serviceAPI;  // return service object
   }]);
